13ffd1c6330ba1a5f81e80f1f9562f6ede35ba77
李俊阳 <2725609187@qq.com>
2024-04-05 11:54:16 +0800

    添加物品描述

--- src/main/java/net/kaupenjoe/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/kaupenjoe/tutorialmod/item/custom/MetalDetectorItem.java
@@ -3,13 +3,19 @@ package net.kaupenjoe.tutorialmod.item.custom;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
 
@@ -56,5 +62,11 @@ public class MetalDetectorItem extends Item {
     //监测铁矿石和钻石矿
     private boolean isValuebleBlock(BlockState state) {
         return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DIAMOND_ORE);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("tooltip.tutorialmod.metal_detector.tooltip"));
+        super.appendTooltip(stack, world, tooltip, context);
     }
 }
