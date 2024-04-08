192e743e3704a0a32f808a8108f0b93473a38282
李俊阳 <2725609187@qq.com>
2024-04-08 19:03:09 +0800

    添加一些红宝石相关建筑方块

--- src/main/java/net/kaupenjoe/tutorialmod/item/ModItemGroups.java
+++ src/main/java/net/kaupenjoe/tutorialmod/item/ModItemGroups.java
@@ -21,10 +21,10 @@ public class ModItemGroups {
 
                         entries.add(ModItems.METAL_DETECTOR);
 
+                        entries.add(Items.DIAMOND);
+
                         entries.add(ModItems.TOMATO);
                         entries.add(ModItems.COAL_BRIQUETTE);
-
-                        entries.add(Items.DIAMOND);
 
                         entries.add(ModBlocks.RUBY_BLOCK);
                         entries.add(ModBlocks.RAW_RUBY_BLOCK);
@@ -36,7 +36,15 @@ public class ModItemGroups {
 
                         entries.add(ModBlocks.SOUND_BLOCK);
 
-
+                        entries.add(ModBlocks.RUBY_STAIRS);
+                        entries.add(ModBlocks.RUBY_SLAB);
+                        entries.add(ModBlocks.RUBY_FENCE);
+                        entries.add(ModBlocks.RUBY_FENCE_GATE);
+                        entries.add(ModBlocks.RUBY_WALL);
+                        entries.add(ModBlocks.RUBY_BUTTON);
+                        entries.add(ModBlocks.RUBY_PRESSURE_PLATE);
+                        entries.add(ModBlocks.RUBY_DOOR);
+                        entries.add(ModBlocks.RUBY_TRAPDOOR);
 
 
                     }).build());
